189abc2f7968b0fac6ecde7c07d299f20f63230d
VinhKhanh <vinhldk@devblock.net>
2021-02-02 14:42:11 +0700

    codebase

---
+++ app/src/main/java/com/example/fabartistcustomer/modal/AppliedJob.java
@@ -0,0 +1,193 @@
+package com.example.fabartistcustomer.modal;
+
+public class AppliedJob {
+    String ajId = "";
+    String userId = "";
+    String artistId = "";
+    String jobId = "";
+    String price = "";
+    String description = "";
+    String status = "";
+    String createdAt = "";
+    String updatedAt = "";
+    String artistImage = "";
+    String artistName = "";
+    String categoryName = "";
+    String categoryPrice = "";
+    String artistAddress = "";
+    String artistMobile = "";
+    String avaRating = "";
+    String artistEmail = "";
+    String currencySymbol = "";
+    String jobDate = "";
+    String time = "";
+    String jobTimestamp = "";
+
+    public String getAjId() {
+        return ajId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getArtistImage() {
+        return artistImage;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public String getCategoryPrice() {
+        return categoryPrice;
+    }
+
+    public String getArtistAddress() {
+        return artistAddress;
+    }
+
+    public String getArtistMobile() {
+        return artistMobile;
+    }
+
+    public String getAvaRating() {
+        return avaRating;
+    }
+
+    public String getArtistEmail() {
+        return artistEmail;
+    }
+
+    public String getCurrencySymbol() {
+        return currencySymbol;
+    }
+
+    public String getJobDate() {
+        return jobDate;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getJobTimestamp() {
+        return jobTimestamp;
+    }
+
+    public void setAjId(String ajId) {
+        this.ajId = ajId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setArtistImage(String artistImage) {
+        this.artistImage = artistImage;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryPrice(String categoryPrice) {
+        this.categoryPrice = categoryPrice;
+    }
+
+    public void setArtistAddress(String artistAddress) {
+        this.artistAddress = artistAddress;
+    }
+
+    public void setArtistMobile(String artistMobile) {
+        this.artistMobile = artistMobile;
+    }
+
+    public void setAvaRating(String avaRating) {
+        this.avaRating = avaRating;
+    }
+
+    public void setArtistEmail(String artistEmail) {
+        this.artistEmail = artistEmail;
+    }
+
+    public void setCurrencySymbol(String currencySymbol) {
+        this.currencySymbol = currencySymbol;
+    }
+
+    public void setJobDate(String jobDate) {
+        this.jobDate = jobDate;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setJobTimestamp(String jobTimestamp) {
+        this.jobTimestamp = jobTimestamp;
+    }
+}
